432514b119fa12ffcc11456c0c437952f01203a2
Miral25 <amiraboubakeresprims@gmail.com>
2025-12-06 22:59:14 +0100

    Fix JSX module handling

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AmiraBoubaker-portfolio/', // Replace with your repo name
+  base: '/AmiraBoubaker-portfolio/',
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  }
 });
